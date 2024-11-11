6f87ed715c336fa786a4b800bdc2129616fcb9ce
root <joshua.becker@ucl.ac.uk>
2024-11-11 09:13:00 +0000

    lightened wiggle

--- ux/tailwind.config.js
+++ ux/tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
     extend: {
       keyframes: {
         wiggle: {
-          '0%, 3%, 12%,  100%': { transform: 'rotate(0deg)' },
-          '1%': { transform: 'rotate(3deg)' },
-          '5%': { transform: 'rotate(-3deg)' },
-          '7%': { transform: 'rotate(3deg)' },
+          '0%, 2%, 5%,  100%': { transform: 'rotate(0deg)' },
+          '1': { transform: 'rotate(3deg)' },
+          '3%': { transform: 'rotate(-3deg)' },
+          '4%': { transform: 'rotate(3deg)' },
         }
       },
       animation: {
-        wiggle: 'wiggle 2s linear infinite'
+        wiggle: 'wiggle 3s linear infinite'
       }
     }
   },
